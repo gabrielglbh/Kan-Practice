142166a9363c57c55819d7246012938823d1d997
Gabriel Garcia <devgglop@gmail.com>
2021-11-16 14:39:01 +0100

    Updated BLOC library

--- lib/ui/pages/backup/bloc/backup_bloc.dart
+++ lib/ui/pages/backup/bloc/backup_bloc.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:kanpractice/core/firebase/queries/back_ups.dart';
@@ -8,34 +7,30 @@ part 'backup_event.dart';
 part 'backup_state.dart';
 
 class BackUpBloc extends Bloc<BackUpEvent, BackUpState> {
-  BackUpBloc() : super(BackUpStateLoaded());
+  BackUpBloc() : super(BackUpStateLoaded()) {
+    on<BackUpLoadingCreateBackUp>((event, emit) async {
+      emit(BackUpStateLoading());
+      final error = await BackUpRecords.instance.createBackUp();
+      if (error == "") emit(BackUpStateLoaded(message: "backup_bloc_creation_successful".tr()));
+      else emit(BackUpStateLoaded(message: "${"backup_bloc_creation_failed".tr()} $error"));
+    });
 
-  @override
-  Stream<BackUpState> mapEventToState(BackUpEvent event) async* {
-    if (event is BackUpLoadingCreateBackUp) yield* _mapCreatedToState();
-    else if (event is BackUpLoadingMergeBackUp) yield* _mapMergedToState(event);
-    else if (event is BackUpLoadingRemoveBackUp) yield* _mapRemovedToState(event);
-    else if (event is BackUpIdle) yield BackUpStateLoaded();
-  }
+    on<BackUpLoadingMergeBackUp>((event, emit) async {
+      emit(BackUpStateLoading());
+      final error = await BackUpRecords.instance.restoreBackUp();
+      if (error == "") emit(BackUpStateLoaded(message: "backup_bloc_merge_successful".tr()));
+      else emit(BackUpStateLoaded(message: "${"backup_bloc_merge_failed".tr()} $error"));
+    });
 
-  Stream<BackUpState> _mapCreatedToState() async* {
-    yield BackUpStateLoading();
-    final error = await BackUpRecords.instance.createBackUp();
-    if (error == "") yield BackUpStateLoaded(message: "backup_bloc_creation_successful".tr());
-    else yield BackUpStateLoaded(message: "${"backup_bloc_creation_failed".tr()} $error");
-  }
+    on<BackUpLoadingRemoveBackUp>((event, emit) async {
+      emit(BackUpStateLoading());
+      final error = await BackUpRecords.instance.removeBackUp();
+      if (error == "") emit(BackUpStateLoaded(message: "backup_bloc_removal_successful".tr()));
+      else emit(BackUpStateLoaded(message: "${"backup_bloc_removal_failed".tr()} $error"));
+    });
 
-  Stream<BackUpState> _mapMergedToState(BackUpLoadingMergeBackUp event) async* {
-    yield BackUpStateLoading();
-    final error = await BackUpRecords.instance.restoreBackUp();
-    if (error == "") yield BackUpStateLoaded(message: "backup_bloc_merge_successful".tr());
-    else yield BackUpStateLoaded(message: "${"backup_bloc_merge_failed".tr()} $error");
-  }
-
-  Stream<BackUpState> _mapRemovedToState(BackUpLoadingRemoveBackUp event) async* {
-    yield BackUpStateLoading();
-    final error = await BackUpRecords.instance.removeBackUp();
-    if (error == "") yield BackUpStateLoaded(message: "backup_bloc_removal_successful".tr());
-    else yield BackUpStateLoaded(message: "${"backup_bloc_removal_failed".tr()} $error");
+    on<BackUpIdle>((event, emit) {
+      emit(BackUpStateLoaded());
+    });
   }
 }
--- lib/ui/pages/test_history/bloc/test_bloc.dart
+++ lib/ui/pages/test_history/bloc/test_bloc.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:kanpractice/core/database/models/test_result.dart';
@@ -8,32 +7,26 @@ part 'test_event.dart';
 part 'test_state.dart';
 
 class TestListBloc extends Bloc<TestListEvent,TestListState> {
-  TestListBloc() : super(TestListStateLoading());
+  TestListBloc() : super(TestListStateLoading()) {
+    on<TestListEventLoading>((event, emit) async {
+      try {
+        emit(TestListStateLoading());
+        final List<Test> list = await TestQueries.instance.getTests(0);
+        emit(TestListStateLoaded(list));
+      } on Exception {
+        emit(TestListStateFailure());
+      }
+    });
 
-  @override
-  Stream<TestListState> mapEventToState(TestListEvent event) async* {
-    if (event is TestListEventLoading) yield* _mapLoadedToState(event);
-    else if (event is TestListEventRemoving) yield* _mapRemovingToState(event);
-  }
-
-  Stream<TestListState> _mapLoadedToState(TestListEventLoading event) async* {
-    try {
-      yield TestListStateLoading();
-      final List<Test> list = await TestQueries.instance.getTests(0);
-      yield TestListStateLoaded(list);
-    } on Exception {
-      yield TestListStateFailure();
-    }
-  }
-
-  Stream<TestListState> _mapRemovingToState(TestListEventRemoving event) async* {
-    try {
-      yield TestListStateLoading();
-      final int code = await TestQueries.instance.removeTests();
-      if (code == 0) yield TestListStateLoaded([]);
-      else TestListStateFailure();
-    } on Exception {
-      yield TestListStateFailure();
-    }
+    on<TestListEventRemoving>((event, emit) async {
+      try {
+        emit(TestListStateLoading());
+        final int code = await TestQueries.instance.removeTests();
+        if (code == 0) emit(TestListStateLoaded([]));
+        else emit(TestListStateFailure());
+      } on Exception {
+        emit(TestListStateFailure());
+      }
+    });
   }
 }

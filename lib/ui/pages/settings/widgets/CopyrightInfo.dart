c674505aa8d26255f8587abab3b7eceeb2e51643
Gabriel Garcia <devgglop@gmail.com>
2021-12-25 16:59:49 +0100

    Changed JMdict to ETLDB

--- lib/ui/pages/settings/widgets/CopyrightInfo.dart
+++ lib/ui/pages/settings/widgets/CopyrightInfo.dart
@@ -18,6 +18,11 @@ class CopyrightInfo extends StatelessWidget {
     );
   }
 
+  Future<void> _launchUrl(BuildContext context, String url) async {
+    if (await canLaunch(url)) await launch(url);
+    else GeneralUtils.getSnackBar(context, "launch_url_failed".tr());
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomSheet(
@@ -47,42 +52,65 @@ class CopyrightInfo extends StatelessWidget {
                       child: RichText(
                         text: TextSpan(
                           children: [
-                            TextSpan(text: "This app uses the ", style: Theme.of(context).textTheme.bodyText2),
+                            TextSpan(text: "“ETL Character Database” is a collection of "
+                                "images of about 1.2 million hand-written and machine-printed "
+                                "numerals, symbols, Latin alphabets and Japanese characters "
+                                "and compiled in 9 datasets (ETL-1 to ETL-9). This database "
+                                "has been collected by Electrotechnical Laboratory (currently "
+                                "reorganized as the ", style: Theme.of(context).textTheme.bodyText2),
                             TextSpan(
-                              text: "JMdict",
+                              text: "National Institute of Advanced Industrial Science and Technology (AIST)",
                               style: TextStyle(color: CustomColors.secondarySubtleColor,
                                   decoration: TextDecoration.underline, fontSize: FontSizes.fontSize14
                               ),
                               recognizer: TapGestureRecognizer()..onTap = () async {
-                                final jmdict = "http://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project";
-                                if (await canLaunch(jmdict)) await launch(jmdict);
-                                else GeneralUtils.getSnackBar(context, "launch_url_failed".tr());
+                                await _launchUrl(context, "https://www.aist.go.jp/index_e.html");
                               },
                             ),
-                            TextSpan(text: " dictionary files. These files are the property of the ",
+                            TextSpan(text: " under the cooperation with Japan Electronic "
+                                "Industry Developement Association (currently reorganized as ",
                               style: Theme.of(context).textTheme.bodyText2),
                             TextSpan(
-                              text: "Electronic Dictionary Research and Development Group",
+                              text: "Japan Electronics and Information Technology Industries Association",
                               style: TextStyle(color: CustomColors.secondarySubtleColor,
                                 decoration: TextDecoration.underline, fontSize: FontSizes.fontSize14
                               ),
                               recognizer: TapGestureRecognizer()..onTap = () async {
-                                final edrdg = "http://www.edrdg.org/";
-                                if (await canLaunch(edrdg)) await launch(edrdg);
-                                else GeneralUtils.getSnackBar(context, "launch_url_failed".tr());
+                                await _launchUrl(context, "https://www.jeita.or.jp/english/");
                               },
                             ),
-                            TextSpan(text: ", and are used in conformance with the Group's  ",
+                            TextSpan(text: "), universities and other research "
+                                "organizations for character recognition researches "
+                                "from 1973 to 1984.",
                               style: Theme.of(context).textTheme.bodyText2),
+                          ]
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: Margins.margin16),
+                      child: RichText(
+                        text: TextSpan(
+                          children: [
                             TextSpan(
-                              text: "licence.",
+                              text: "About the ETL Character Database",
                               style: TextStyle(color: CustomColors.secondarySubtleColor,
-                                decoration: TextDecoration.underline, fontSize: FontSizes.fontSize14
+                                  decoration: TextDecoration.underline, fontSize: FontSizes.fontSize14
                               ),
                               recognizer: TapGestureRecognizer()..onTap = () async {
-                                final license = "http://www.edrdg.org/edrdg/licence.html";
-                                if (await canLaunch(license)) await launch(license);
-                                else GeneralUtils.getSnackBar(context, "launch_url_failed".tr());
+                                await _launchUrl(context, "http://etlcdb.db.aist.go.jp/");
+                              },
+                            ),
+                            TextSpan(text: ". From all the 9 datasets, the one used "
+                                "in this app is ",
+                                style: Theme.of(context).textTheme.bodyText2),
+                            TextSpan(
+                              text: "ETL-9.",
+                              style: TextStyle(color: CustomColors.secondarySubtleColor,
+                                  decoration: TextDecoration.underline, fontSize: FontSizes.fontSize14
+                              ),
+                              recognizer: TapGestureRecognizer()..onTap = () async {
+                                await _launchUrl(context, "http://etlcdb.db.aist.go.jp/specification-of-etl-9");
                               },
                             ),
                           ]
